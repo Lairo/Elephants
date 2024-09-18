1a43a4c3fedb812d3c85eaa640989d1fd23f5af8
chajus1 <chajus1@googlemail.com>
2024-09-18 19:59:30 +0100

    EarSize

--- Elephants/Elephant.cs
+++ Elephants/Elephant.cs
@@ -10,12 +10,29 @@ namespace Elephants
     {
         public string Name = "";
         public int EarSize;
+        
 
+        /// <summary>
+        /// The elephant will name itself and give you the size its ears.
+        /// "choice" is the number choice inside the loop 1,2,3
+        /// </summary>
+        /// <param name="choice"> </param>
         public void WhoAmI(char choice)
         {
             Console.WriteLine($"You pressed {choice}");
             Console.WriteLine($"Calling {Name.ToLower()}.WhoAmI()");
             Console.WriteLine($"My name is {Name}.\nMy ears are {EarSize} inches tall.\n");
+        }
+
+        internal void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine($"{Name} hear a message");
+            Console.WriteLine($"{whoSaidIt.Name} said this: {message}");
         }
     }
 }
--- Elephants/Program.cs
+++ Elephants/Program.cs
@@ -2,36 +2,71 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Elephant lloyd = new () { Name = "Lloyd", EarSize = 40 };
-            Elephant lucinda = new() { Name = "Lucinda", EarSize = 33 };
-            Elephant swap = new() { Name = "", EarSize = 0 };
+            Elephant[] elephants = new Elephant[7];
+            elephants[0] = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            elephants[1] = new Elephant() { Name = "Lucinda", EarSize = 33};
+            elephants[2] = new Elephant() { Name = "Larry", EarSize = 42 };
+            elephants[3] = new Elephant() { Name = "Lucille", EarSize = 32 };
+            elephants[4] = new Elephant() { Name = "Lars", EarSize = 44 };
+            elephants[5] = new Elephant() { Name = "Linda", EarSize = 33 };
+            elephants[6] = new Elephant() { Name = "Humphrey", EarSize = 45 };
 
-            while (true)
+            Elephant biggestEars = elephants[0];
+
+            for(int i = 1; i < elephants.Length; i++)
             {
-                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
-                char keyChar = Console.ReadKey(true).KeyChar;
-                if (keyChar == '1')
-                {                    
-                    lloyd.WhoAmI(keyChar);
-                } else if (keyChar == '2')
+                Console.WriteLine($"Iteration #{i}");
+
+                if (elephants[i].EarSize > biggestEars.EarSize)
                 {
-                    lucinda.WhoAmI(keyChar);
-                }else if (keyChar == '3')
-                {
-                    swap = lloyd;
-                    lloyd = lucinda;
-                    lucinda = swap;
-                    Console.WriteLine("\nReferences have been swapped.\n");
+                    biggestEars = elephants[i];
                 }
-                else
-                {
-                    return;
-                }
+
+                Console.WriteLine(biggestEars.EarSize.ToString());
+            }
+
+
+            //Elephant lloyd = new() { Name = "Lloyd", EarSize = 40 };
+            //Elephant lucinda = new() { Name = "Lucinda", EarSize = 33 };
+            //Elephant swap = new();
+
+            //bool[] myArray = new bool[15];
+            //myArray[4] = false;
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            //    char keyChar = Console.ReadKey(true).KeyChar;
+            //    if (keyChar == '1')
+            //    {                    
+            //        lloyd.WhoAmI(keyChar);
+            //    } else if (keyChar == '2')
+            //    {
+            //        lucinda.WhoAmI(keyChar);
+            //    }else if (keyChar == '3')
+            //    {
+            //        swap = lloyd;
+            //        lloyd = lucinda;
+            //        lucinda = swap;
+            //        Console.WriteLine("\nReferences have been swapped.\n");
+            //    } else if (keyChar == '4')
+            //    {
+            //        lloyd = lucinda;
+            //        lloyd.EarSize = 4321;
+            //        lloyd.WhoAmI(keyChar);
+            //    } else if (keyChar == '5')
+            //    {
+            //        lucinda.SpeakTo(lloyd, "Hi, Lloyd!");
+            //    }
+            //    else
+            //    {
+            //        return;
+            //    }
 
                 //Console.WriteLine("Press anything else to quit or make another choice.\n");
-            }
+            //}
         }
     }
 }
