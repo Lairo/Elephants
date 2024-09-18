f82773433633fe327243ba2c9e77e2cfffb1d54d
chajus1 <chajus1@googlemail.com>
2024-09-18 15:32:46 +0100

    swapper

--- Elephants/Elephant.cs
+++ Elephants/Elephant.cs
@@ -8,12 +8,14 @@ namespace Elephants
 {
     internal class Elephant
     {
-        string Name = "";
-        int EarSize;
+        public string Name = "";
+        public int EarSize;
 
-        public void WhoAmI()
+        public void WhoAmI(char choice)
         {
-
+            Console.WriteLine($"You pressed {choice}");
+            Console.WriteLine($"Calling {Name.ToLower()}.WhoAmI()");
+            Console.WriteLine($"My name is {Name}.\nMy ears are {EarSize} inches tall.\n");
         }
     }
 }
--- Elephants/Program.cs
+++ Elephants/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Elephant[] elephants = new Elephant[7];
+            Elephant lloyd = new () { Name = "Lloyd", EarSize = 40 };
+            Elephant lucinda = new() { Name = "Lucinda", EarSize = 33 };
+            Elephant swap = new() { Name = "", EarSize = 0 };
+
+            while (true)
+            {
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (keyChar == '1')
+                {                    
+                    lloyd.WhoAmI(keyChar);
+                } else if (keyChar == '2')
+                {
+                    lucinda.WhoAmI(keyChar);
+                }else if (keyChar == '3')
+                {
+                    swap = lloyd;
+                    lloyd = lucinda;
+                    lucinda = swap;
+                    Console.WriteLine("\nReferences have been swapped.\n");
+                }
+                else
+                {
+                    return;
+                }
+
+                //Console.WriteLine("Press anything else to quit or make another choice.\n");
+            }
         }
     }
 }
